640b129aa19417f489b47dba466039ee09189fd0
Jonathan Forney <jonathan.forney@gmail.com>
2019-07-13 23:24:09 -0700

    updated map to remove c log

--- src/map/map.js
+++ src/map/map.js
@@ -5,7 +5,7 @@ import createQuestLink from '../quest/create-quest-link.js';
 loadProfile();
 
 const quests = api.getQuests();
-const user = api.getUser();
+// const user = api.getUser();
 const section = document.getElementById('missions');
 
 for(let i = 0; i < quests.length; i++) {
